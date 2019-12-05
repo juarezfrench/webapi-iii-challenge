2180c13890618d1adbd4a2537f16bc701af56ca1
juarezfrench <markedfrench@gmail.com>
2019-12-05 14:48:49 -0600

    Set up routes in server file

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,19 +3,48 @@ const express = 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    router.get('/', (req, res) => {
+        Posts.get(req.query)
+          .then(posts => res.status(200).json(posts))
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: 'Error retrieving the posts' });
+          });
 });
 
 router.get('/:id', (req, res) => {
-
+    router.get('/:id', validatePostId, (req, res) => {
+        Posts.getById(req.params.id)
+          .then(post => res.status(200).json(post))
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: 'Error retrieving the post' });
+          });
+      });
 });
 
 router.delete('/:id', (req, res) => {
-
+    router.delete('/:id', validatePostId, (req, res) => {
+        Posts.remove(req.params.id)
+          .then(count =>
+            res.status(200).json({ message: 'The post has been deleted' }),
+          )
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: 'Error removing the post' });
+          });
+      });
 });
 
 router.put('/:id', (req, res) => {
-
+    router.put('/:id', validatePostId, (req, res) => {
+        Posts.update(req.params.id, req.body)
+          .then(post => res.status(200).json(post))
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: 'Error updating the post' });
+          });
+      });
 });
 
 // custom middleware
